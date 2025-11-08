a2bf14218dca5c5a433e0d04c7af38563ade127c
AjwadIbnSwalehin <aibnswalehin@gmail.com>
2025-11-08 21:19:43 +0000

    Added planet list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import PlanetsGroup from "./components/PlanetsGroup";
 
 function App() {
-  return <div><PlanetsGroup></PlanetsGroup></div>
+  let planets = [
+    "Isstvan III",
+    "Davin",
+    "Olympia",
+    "Segmentum Ultima",
+    "Tarsus",
+    "Arigatta",
+    "Prospero",
+    "Mars",
+    "Isstvan V",
+    "Tallarn",
+    "Belt of Iron",
+    "Pluto",
+    "Terra",
+  ];
+  return (
+    <div>
+      <PlanetsGroup planets={planets} heading="Planets"></PlanetsGroup>
+    </div>
+  );
 }
 
 export default App;
--- src/components/PlanetsGroup.tsx
+++ src/components/PlanetsGroup.tsx
@@ -1,26 +1,33 @@
-function PlanetsGroup() {
-  const planets = [
-    "Isstvan III",
-    "Davin",
-    "Olympia",
-    "Segmentum Ultima",
-    "Tarsus",
-    "Arigatta",
-    "Prospero",
-    "Mars",
-    "Isstvan V",
-    "Tallarn",
-    "Belt of Iron",
-    "Pluto",
-    "Terra",
-  ];
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+interface Props {
+  planets: string[];
+  heading: string;
+}
+
+function PlanetsGroup({ planets, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>Planets</h1>
+      <h1>{heading}</h1>
+      {planets.length === 0 && <p>No planets found.</p>}
       <ul className="list-group">
-        {planets.map((planet) => (
-          <li key={planet}>{planet}</li>
+        {planets.map((planet, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={planet}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {planet}
+          </li>
         ))}
       </ul>
     </>
