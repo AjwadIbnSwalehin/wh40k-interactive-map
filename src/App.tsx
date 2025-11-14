a575136dcaa6b0f49e5ac0990238c3d0621062a6
AjwadIbnSwalehin <aibnswalehin@gmail.com>
2025-11-14 12:35:45 +0000

    Added the Isstvan planets

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./App.css";
 import Planet from "./components/Planet";
 import terraImg from "./assets/Terra.png";
 import marsImg from "./assets/Mars.png";
+import isstvaniiiImg from "./assets/Isstvan_III.png";
+import isstvanvImg from "./assets/isstvan_V.png";
 
 const planets = [
-  { name: "Terra", image: terraImg, x: 500, y: 300 },
-  { name: "Mars", image: marsImg, x: 800, y: 150 },
+  { name: "Terra", image: terraImg, x: 750, y: 350 },
+  { name: "Mars", image: marsImg, x: 750, y: 250 },
+  { name: "Isstvan III", image: isstvaniiiImg, x: 1400, y: 100 },
+  { name: "Isstvan V", image: isstvanvImg, x: 1300, y: 100 },
 ];
 
 const generateStars = (numStars: number) => {
--- src/components/Terra.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import terra from "../assets/Terra.png";
-
-const Terra = () => {
-  return <img src={terra} className="terra-image" alt="Terra" />;
-};
-
-export default Terra;
