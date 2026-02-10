597fb242ff7f61f6ccc809df421ab973a972f2ed
AjwadIbnSwalehin <aibnswalehin@gmail.com>
2026-02-10 12:40:21 +0000

    Added new planets

--- src/layout/usePlanets.ts
+++ src/layout/usePlanets.ts
@@ -3,6 +3,8 @@ import marsImg from "../assets/Mars.png";
 import isstvaniiiImg from "../assets/Isstvan_III.png";
 import isstvanvImg from "../assets/isstvan_V.png";
 import davinImg from "../assets/Davin.png";
+import olympiaImg from "../assets/Olympia.png";
+import prosperoImg from "../assets/Prospero.png";
 
 
 const usePlanets = () => {
@@ -11,7 +13,9 @@ const usePlanets = () => {
   { name: "Mars", image: marsImg, x: 750, y: 250 },
   { name: "Isstvan III", image: isstvaniiiImg, x: 1400, y: 100 },
   { name: "Isstvan V", image: isstvanvImg, x: 1300, y: 100 },
-  { name: "Davin", image: davinImg, x: 1000, y: 350 },
+  { name: "Davin", image: davinImg, x: 900, y: 350 },
+  { name: "Olympia", image: davinImg, x: 1050, y: 350 },
+  { name: "Prospero", image: davinImg, x: 400, y: 200 },
 ];
 
   return planets;
