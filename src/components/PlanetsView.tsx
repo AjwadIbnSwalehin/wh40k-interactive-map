9b59b126513203ca8fab7128859356763c928b78
AjwadIbnSwalehin <aibnswalehin@gmail.com>
2026-02-12 16:59:31 +0000

    Removed unnecessary imports

--- src/components/PlanetsView.tsx
+++ src/components/PlanetsView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Planet from "./Planet";
 import useStars from "../layout/useStars";
 import usePlanets from "../layout/usePlanets";
